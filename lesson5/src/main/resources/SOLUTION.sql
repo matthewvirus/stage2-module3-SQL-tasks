f194fe1da87e2a35016f2a920895e7ec14bc8ba4
Matthew Zhigalo <matthewzhigalo@email.com>
2023-03-23 15:46:36 +0300

    Fixed current_date error

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM Payment WHERE amount >= 500;
-SELECT * FROM Student WHERE DATEADD(year, -20, 20230323) > birthday;
-SELECT * FROM Student WHERE groupnumber = 10 AND DATEADD(YEAR, -20, 20230323) < birthday;
+SELECT * FROM Student WHERE DATEADD(year, -20, CURRENT_DATE) > birthday;
+SELECT * FROM Student WHERE groupnumber = 10 AND DATEADD(YEAR, -20, CURRENT_DATE) < birthday;
 SELECT * FROM Student WHERE name = 'Mike' OR groupnumber IN (4,5,6);
-SELECT * FROM Payment WHERE DATEADD(month, -8, 20230323) < payment_date;
+SELECT * FROM Payment WHERE DATEADD(month, -8, CURRENT_DATE) < payment_date;
 SELECT * FROM Student WHERE name LIKE 'A%';
 SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9)
