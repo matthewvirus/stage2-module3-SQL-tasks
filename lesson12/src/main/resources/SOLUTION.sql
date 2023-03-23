f0059ab736098a408653c227ab3deff71259ed54
Matthew Zhigalo <matthewzhigalo@email.com>
2023-03-23 15:51:55 +0300

    Solved

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM Student WHERE id IN (SELECT DISTINCT mark.student_id from Mark join Subject S on S.id = Mark.subject_id WHERE grade >= 4);
+DELETE FROM Student WHERE id IN (SELECT DISTINCT mark.student_id FROM mark JOIN subject ON subject.id = mark.subject_id WHERE grade >= 4);
 DELETE FROM Student WHERE id IN (SELECT DISTINCT student.id from Student join Mark M on Student.id = M.student_id WHERE mark < 4);
 DELETE FROM PaymentType WHERE name = 'Daily';
 DELETE FROM Mark WHERE mark < 7;
